676f1613386784c0be21456dc351a6c595331dc9
Tarunkumar5129 <jangamk.ug21.ec@nitp.ac.in>
2025-05-16 01:55:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
+app.get("/api/health", (req, res) => res.json({ status: "OK" }));
 app.listen(port, () => {});
